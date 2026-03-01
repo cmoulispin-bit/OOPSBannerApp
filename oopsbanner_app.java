479f2b49aeaaf0541a4370c950050a9f2c065564
cm123 <cmoulispin@gmail.com>
2026-03-01 20:02:55 +0530

    uc5 coded

--- oopsbanner_app.java
+++ oopsbanner_app.java
@@ -1,37 +1,41 @@
 public class oopsbanner_app {
     public static void main(String[] args){
-        String[] line1 = {
-            "   ***   ", "   ***   ", "******   ", "   ***** "
+        
+        String[][] bannerLines = { 
+            {
+                "   ***   ", "   ***   ", "******   ", "   ***** "
+            },
+            {
+                " **   ** ", " **   ** ", "**    ** ", " **      "
+            },
+            {
+                "**     **", "**     **", "**     **", "**       "
+            },
+            {
+                "**     **", "**     **", "**    ** ", " **      "
+            },
+            {
+                "**     **", "**     **", "******   ", "   ***   "
+            },
+            {
+                "**     **", "**     **", "**       ", "      ** "
+            },
+            {
+                "**     **", "**     **", "**       ", "       **"
+            },
+            {
+                " **   ** ", " **   ** ", "**       ", "      ** "
+            },
+            {
+                "   ***   ", "   ***   ", "**       ", " ******  "
+            }
         };
-        String[] line2 = {
-            " **   ** ", " **   ** ", "**    ** ", " **      "
-        };
-        String[] line3 = {
-            "**     **", "**     **", "**     **", "**       "
-        };
-        String[] line4 = {
-            "**     **", "**     **", "**    ** ", " **      "
-        };
-        String[] line5 = {
-            "**     **", "**     **", "******   ", "   ***   "
-        };
-        String[] line6 = {
-            "**     **", "**     **", "**       ", "      ** "
-        };
-        String[] line7 = {
-            "**     **", "**     **", "**       ", "       **"
-        };
-        String[] line8 = {
-            " **   ** ", " **   ** ", "**       ", "      ** "
-        };
-        String[] line9 = {
-            "   ***   ", "   ***   ", "**       ", " ******  "
-        };
-        String[][] bannerLines = {
-            line1, line2, line3, line4, line5, line6, line7, line8, line9
-        };
+
         for (String[] line : bannerLines) {
-            System.out.println(String.join(" ", line));
+            for (String segment : line) {
+                System.out.print(segment + " ");
+            }
+            System.out.println();
         }
     }
     
