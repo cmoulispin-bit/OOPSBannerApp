42d3b781899507f9847201c36627e51d274c97ff
cm123 <cmoulispin@gmail.com>
2026-03-02 21:32:38 +0530

    uc6 coded

--- oopsbanner_app.java
+++ oopsbanner_app.java
@@ -1,42 +1,47 @@
 public class oopsbanner_app {
-    public static void main(String[] args){
-        
-        String[][] bannerLines = { 
-            {
-                "   ***   ", "   ***   ", "******   ", "   ***** "
-            },
-            {
-                " **   ** ", " **   ** ", "**    ** ", " **      "
-            },
-            {
-                "**     **", "**     **", "**     **", "**       "
-            },
-            {
-                "**     **", "**     **", "**    ** ", " **      "
-            },
-            {
-                "**     **", "**     **", "******   ", "   ***   "
-            },
-            {
-                "**     **", "**     **", "**       ", "      ** "
-            },
-            {
-                "**     **", "**     **", "**       ", "       **"
-            },
-            {
-                " **   ** ", " **   ** ", "**       ", "      ** "
-            },
-            {
-                "   ***   ", "   ***   ", "**       ", " ******  "
-            }
-        };
+    
+    public static String[] getOpattern() {
+        String[] lines =   {"   ***    ",
+                            " **   **  ",
+                            "**     ** ",
+                            "**     ** ",
+                            "**     ** ",
+                            "**     ** ",
+                            "**     ** ",
+                            "   ***    "};
+        return lines;
+    }
+    public static String[] getPpattern() {
+        String[] lines =  {"******   ",
+                        "**    ** ",
+                        "**    ** ",
+                        "******   ",
+                        "**       ",
+                        "**       ",
+                        "**       ",
+                        "**       ",
+                        "**       "};
 
-        for (String[] line : bannerLines) {
-            for (String segment : line) {
-                System.out.print(segment + " ");
-            }
-            System.out.println();
+        return lines;
+    }
+    public static String[] getSpattern() {
+        String[] lines = {"   *****  ",
+                        " **      ",
+                        "**       ",
+                        "   ***   ",
+                        "      ** ",
+                        "      ** ",
+                        " **   ** ",
+                        "  *****  "};
+        return lines;
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOpattern();
+        String[] pPattern = getPpattern();
+        String[] sPattern = getSpattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + oPattern[i]+ pPattern[i]  + sPattern[i]);
         }
     }
-    
 }
